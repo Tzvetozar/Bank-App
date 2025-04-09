7b1667d4fb002967bcb634675f5d5019f4227850
Tzvetozar <tzvetozar123@yahoo.co.uk>
2025-04-09 21:36:50 +0300

    Creation of LoginGui, BaseFrame and AppLauncher classes

---
+++ src/guis/LoginGui.java
@@ -0,0 +1,17 @@
+package guis;
+
+/*
+   This Gui will allow user to login or launch the register gui
+   This extends from the BaseFrame which emans we will need to define our own addGuiComponent()
+ */
+
+public class LoginGui extends BaseFrame {
+    public LoginGui() {
+        super("Banking App Login");
+    }
+
+    @Override
+    protected void addGuiComponents() {
+
+    }
+}
