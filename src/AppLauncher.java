6e203ddf86b5fa7c5e2b570380a6be2147f61aad
Tzvetozar <tzvetozar123@yahoo.co.uk>
2025-04-23 07:36:02 +0300

    Setting GUI sizes

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -1,2 +1,14 @@
+import guis.LoginGui;
+import javax.swing.*;
+
 public class AppLauncher {
+    public static void main(String[] args) {
+        // useinvokeLater to make updates to the GUI more thread-safe
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LoginGui().setVisible(true);
+            }
+        });
+    }
 }
